f7f4dcac800ffd958b06ee9373f52aaedfde7855
baoYang <1411091515@qq.com>
2019-11-26 23:15:02 +0800

    feat:commit readMe config.

--- cloud-weather-config-client/src/main/java/com/yang/config/client/ConfigClientApplication.java
+++ cloud-weather-config-client/src/main/java/com/yang/config/client/ConfigClientApplication.java
@@ -1,8 +1,11 @@
 package com.yang.config.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author: Mu_Yi
@@ -13,10 +16,19 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class ConfigClientApplication {
+
+    @Value("${auther}")
+    private String auther;
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class,args);
+    }
+
+    @GetMapping("/auther")
+    public Object auther(){
+        return auther;
     }
 
 }
